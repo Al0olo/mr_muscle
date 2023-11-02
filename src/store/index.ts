7a7a0df54f1e129d4e74c2986a40e1388f4086a6
Al0olo <99741501+Al0olo@users.noreply.github.com>
2023-11-02 11:18:28 +0200

    Create users Object in vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+      users: [
+          { id: '1', name: 'Mahmoud Abdelsalam', email: '7oda@gmail.com' }
+      ]
   },
   getters: {
+    users (state) {
+      // state variable contains our state data
+      return state.users;
+  }
   },
   mutations: {
   },
