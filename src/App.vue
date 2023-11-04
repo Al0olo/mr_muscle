b06055a24a435e1103df7f426131961e5cf8c23f
Al0olo <99741501+Al0olo@users.noreply.github.com>
2023-11-04 04:15:56 +0200

    Create User Profile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> | 
+    <router-link to="/register">Create Member</router-link> 
   </nav>
   <router-view/>
 </template>
