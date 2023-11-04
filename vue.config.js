205c8a69ed51bc7de95ff1ae4f25b15cef5e287a
Al0olo <99741501+Al0olo@users.noreply.github.com>
2023-11-04 04:53:09 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,22 +3,22 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-module.exports = {
-  pwa: {
-    manifestOptions: {
-      name: "MR Muscle",
-      short_name: "MR Muscle",
-      start_url: "./",
-      display: "standalone",
-      theme_color: "#000000",
-      icons: [
-        {
-          src: "./favicon.svg",
-          sizes: "512x512",
-          type: "image/svg+xml",
-          purpose: "any maskable",
-        },
-      ],
-    },
-  },
-};
+// module.exports = {
+//   pwa: {
+//     manifestOptions: {
+//       name: "MR Muscle",
+//       short_name: "MR Muscle",
+//       start_url: "./",
+//       display: "standalone",
+//       theme_color: "#000000",
+//       icons: [
+//         {
+//           src: "./favicon.svg",
+//           sizes: "512x512",
+//           type: "image/svg+xml",
+//           purpose: "any maskable",
+//         },
+//       ],
+//     },
+//   },
+// };
